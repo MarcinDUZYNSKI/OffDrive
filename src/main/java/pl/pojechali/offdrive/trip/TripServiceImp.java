9d369dbba5e7fd7819606546466851f1879349ec
MarcinDUZYNSKI <marcin@pojehcali.pl>
2020-09-23 23:24:35 +0200

    trip & TripCondition controller & view save success with user ID

--- src/main/java/pl/pojechali/offdrive/trip/TripServiceImp.java
+++ src/main/java/pl/pojechali/offdrive/trip/TripServiceImp.java
@@ -1,9 +1,12 @@
 package pl.pojechali.offdrive.trip;
 
 import lombok.Data;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import pl.pojechali.offdrive.tripCondition.TripCondition;
 import pl.pojechali.offdrive.tripCondition.TripConditionsRepository;
+import pl.pojechali.offdrive.user.UserRepository;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
@@ -14,10 +17,20 @@ import java.time.LocalDateTime;
 public class TripServiceImp implements TripService {
     private final TripRepository tripRepository;
     private final TripConditionsRepository tripConditionsRepository;
+    private final UserRepository userRepository;
 
     @Override
     public void saveTrip(Trip trip) {
+        String username;
         trip.setTripDate(LocalDateTime.now());
+        //get current login user
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+             username = ((UserDetails)principal).getUsername();
+        } else {
+             username = principal.toString();
+        }
+        trip.setUser(userRepository.findUserByEmail(username));
         tripRepository.save(trip);
     }
 
