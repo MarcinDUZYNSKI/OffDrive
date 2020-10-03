8ab34c2277d99a2b653a1be62c330c20ba19d0d5
AnetaDuzynska <a.d.duzynska@gmail.com>
2020-10-03 14:40:31 +0200

    email sender

--- src/main/java/pl/pojechali/offdrive/OffdriveApplication.java
+++ src/main/java/pl/pojechali/offdrive/OffdriveApplication.java
@@ -1,13 +1,32 @@
 package pl.pojechali.offdrive;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootApplication
 public class OffdriveApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OffdriveApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(OffdriveApplication.class, args);
+  }
+
+  @Autowired(required = false)
+  private JavaMailSender mailSender;
+
+  @Bean
+  public CommandLineRunner springMail() {
+    return args -> {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setSubject("Create Account"); // tu wpisac tekst
+        message.setFrom("offdrive@gmail.com");
+        message.setText("Please use this link to authorise account in OffDrive");
+        mailSender.send(message);
+    };
+  }
 
 }
