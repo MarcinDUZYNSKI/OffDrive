ada7402f253a31d0b90354b1a5bbc630bd7f2317
AnetaDuzynska <a.d.duzynska@gmail.com>
2020-10-03 15:20:01 +0200

    test email sender

--- src/main/java/pl/pojechali/offdrive/OffdriveApplication.java
+++ src/main/java/pl/pojechali/offdrive/OffdriveApplication.java
@@ -1,5 +1,12 @@
 package pl.pojechali.offdrive;
 
+import com.sun.tools.javac.util.List;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +14,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 @SpringBootApplication
 public class OffdriveApplication {
@@ -21,12 +31,31 @@ public class OffdriveApplication {
   @Bean
   public CommandLineRunner springMail() {
     return args -> {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setSubject("Create Account"); // tu wpisac tekst
-        message.setFrom("offdrive@gmail.com");
-        message.setText("Please use this link to authorise account in OffDrive");
-        mailSender.send(message);
+      SimpleMailMessage message = new SimpleMailMessage();
+      message.setSubject("Create Account"); // tu wpisac tekst
+      message.setFrom("offdrive@gmail.com");
+      message.setText("Please use this link to authorise account in OffDrive");
+      mailSender.send(message);
+
+      FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+      freeMarkerConfigurer.setTemplateLoaderPath("/WEB-INF/mail/templates");
+      Configuration configuration = freeMarkerConfigurer.createConfiguration();
+
+      Template mailTemplate = configuration.getTemplate("test-mail.html");
+      Map<String, Object> model = new HashMap<>();
+      model.put("username", "joeSmith");
+      model.put("today", LocalDate.now());
+      model.put("orders", List.of("Lala", "Piłka"));
+
+
+      String mailBody = FreeMarkerTemplateUtils.processTemplateIntoString(mailTemplate, null);
+      message.setText(mailBody);
+
+      MimeMessage mimeMessage = mailSender.createMimeMessage();
+      MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+      mailSender.send(message);
     };
+
   }
 
 }
--- src/main/webapp/WEB-INF/views/admin/login.jsp
+++ src/main/webapp/WEB-INF/views/admin/login.jsp
@@ -27,6 +27,7 @@
                     <div><label> Email: <input type="text" name="username"/> </label></div>
                     <div><label> Password: <input type="password" name="password"/> </label></div>
                     <div><input type="submit" value="Login"/></div>
+                    <a href="/reset_password">I forgot my password</a>
                     <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
                 </form>
             </div>
