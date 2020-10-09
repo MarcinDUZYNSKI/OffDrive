7ed4c4daee5d23478072cedc17755485a88b5f3d
MarcinDUZYNSKI <marcin@pojehcali.pl>
2020-10-09 17:39:43 +0200

    user cerrected

--- src/main/java/pl/pojechali/offdrive/user/UserController.java
+++ src/main/java/pl/pojechali/offdrive/user/UserController.java
@@ -1,11 +1,17 @@
 package pl.pojechali.offdrive.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import pl.pojechali.offdrive.exception.UserAlreadyExistException;
 import pl.pojechali.offdrive.trip.Trip;
@@ -69,13 +75,22 @@ public class UserController {
         return "admin/deleteConfirm";
     }
     @RequestMapping("/index/approvedUserDelete")
-    public String deleteUser() {
+    public String deleteUser(HttpServletRequest request, HttpServletResponse response) {
         User user = userService.getCurrentLoginUser();
         if(user == null) {
             return "admin/403";
         }
+
+
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if (auth != null) {
+                new SecurityContextLogoutHandler().logout(request, response, auth);
+            }
+
         userService.deleteUser(user);
-        return "redirect:/perform_logout";
+            return "redirect:/";
+
+//        return "redirect:/perform_logout";
     }
 
     @RequestMapping("/index/userProfile")
--- src/main/java/pl/pojechali/offdrive/user/UserUtils.java
+++ src/main/java/pl/pojechali/offdrive/user/UserUtils.java
@@ -1,9 +1,14 @@
 package pl.pojechali.offdrive.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,5 +23,13 @@ public class UserUtils {
             username = principal.toString();
         }
         return (username);
+    }
+
+    public String userLogout (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
     }
 }
